ac37f62473650fa2aea91ba47f21af8249c03032
aeronbaon1-cmyk <baon.412846@balayan.sti.edu.ph>
2026-09-14 18:55:24 +0800

    Update student profile output.

--- StudentProfile/Form1.Designer.cs
+++ StudentProfile/Form1.Designer.cs
@@ -36,9 +36,9 @@
             label1.AutoSize = true;
             label1.Location = new Point(129, 66);
             label1.Name = "label1";
-            label1.Size = new Size(246, 20);
+            label1.Size = new Size(271, 20);
             label1.TabIndex = 0;
-            label1.Text = "Student Profile — Allan Aeron Baon";
+            label1.Text = "Student Profile — GitHub Beginner Lab.";
             // 
             // Form1
             // 
